07e411d0fd6309b97bd7cc6c0f5c16ffc0e865fa
aviavi <avizivald@gmail.com>
2020-12-22 00:24:23 +0200

    conect to another site2

--- src/Tfila.js
+++ src/Tfila.js
@@ -1,11 +1,11 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import Shul from './utils/Shul';
 const axios = require('axios');
 
 function Tfila() {
-  let newShuls=[];
-  const [shuls,setShuls] = useState([]);
-  function a (){
+  let newShuls = [];
+  const [shuls, setShuls] = useState([]);
+  function a() {
     setShuls(newShuls)
   }
   function ax() {
@@ -13,32 +13,32 @@ function Tfila() {
     if (window.location.host === 'localhost:3000') {
       host = 'http://localhost:3001/'
     }
-    else{
+    else {
       host = 'https://myfreeserver.herokuapp.com/'
     }
     console.log('aaaaaaaaaaaaaaaaaaa');
     axios.get(`${host}`)
       .then(function (response) {
-        console.log('location.hostname====>>.  ',window.location.hostname);
-        console.log('location.====>>.  ',window.location);
-        console.log('response==...>>>  ',response);
-        response.data.forEach(element => {
-          newShuls.push(/*<Shul shul={element} />*/element)
-        })
+        console.log('location.hostname====>>.  ', window.location.hostname);
+        console.log('location.====>>.  ', window.location);
+        console.log('response==...>>>  ', response);
+        // response.data.forEach(element => {
+          newShuls.push(/*<Shul shul={element} />*/response.data)
+        // })
         setShuls(newShuls)
-        
+
         // handle success
         console.log(response);
       })
       .catch(function (error) {
-        console.log('location.hostname====>>.  ',window.location.hostname);
-        console.log('location.====>>.  ',window.location);
+        console.log('location.hostname====>>.  ', window.location.hostname);
+        console.log('location.====>>.  ', window.location);
         // handle error
         console.log(error);
       })
-    }
+  }
 
- 
+
   return (
     <div>
       <button onClick={ax}>ax</button>
