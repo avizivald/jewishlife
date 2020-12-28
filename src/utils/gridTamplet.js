deb92db1fbc4e809507b1c6b5badc9e760255aa2
aviavi <avizivald@gmail.com>
2020-12-28 14:47:01 +0200

    new grid tamplet

---
+++ src/utils/gridTamplet.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './gridTamplet.css'
+
+function gridTamplet(props) {
+    return (
+        <div class="grid-container">
+        <div class="main"></div>
+        <div class="header"></div>
+        <div class="footer"></div>
+        <div class="r2"></div>
+        <div class="r1"></div>
+        <div class="r3"></div>
+        <div class="l1"></div>
+        <div class="l2"></div>
+        <div class="l3"></div>
+        <div class="ml"></div>
+        <div class="mr"></div>
+      </div>
+    );
+}
+export default gridTamplet;
+
+
+
