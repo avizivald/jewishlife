3773681ee5f5c0d8a4a8f804f2fe8cafaa948133
aviavi <avizivald@gmail.com>
2021-01-10 15:49:43 +0200

    new app

---
+++ src/utils/Cell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cell.css';
+
+function Cell(props) {
+    // const { arvit, mincha, shacharit, name } = props.
+
+    return (
+        <div className='cell'>
+
+        </div>
+
+    );
+}
+export default Cell;
