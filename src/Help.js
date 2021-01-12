f4d9c435529dd847a9a62fe0ac4c26e1e68d50bf
aviavi <avizivald@gmail.com>
2021-01-12 11:00:08 +0200

    secend commit

--- src/Help.js
+++ src/Help.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import GridTamplet from './utils/GridTamplet';
+// import GridTamplet from './utils/GridTamplet';
 function Help() {
     return (
-        <GridTamplet />
+        // <GridTamplet />
+        <p>papapapapapapapa</p>
     );
 }
 export default Help;
